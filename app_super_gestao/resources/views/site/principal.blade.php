2de16f273b342a49b35f17dcf474df93f9f288c1
Vitor <vitorcostadev@gmail.com>
2023-03-24 16:06:41 -0300

    aprendendo sobre rotas e parametros

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -5,9 +5,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>View Principal</title>
 </head>
+
 <body>
     <h3>Principal (view)</h3>
+    <ul>
+        <li>
+            <a href="{{ route('site.index') }}">
+                Principal
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('site.sobrenos') }}">
+                Sobre Nós
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('site.contato') }}">
+                Contato
+            </a>
+        </li>
+    </ul>
 </body>
+
 </html>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,13 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return "Olá, seja bem vindo ao curso!";
-// });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
 
-Route::get('/sobre-nos','SobreNosController@sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+// nome, categoria, assunto, mensagem
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
 
-Route::get('/contato','ContatoController@contato');
+// app 
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
+});
 
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe, 
+    <a href="' . route('site.index') . '">clique aqui</a>
+     para ir para a página 
+    inicial';
+});
