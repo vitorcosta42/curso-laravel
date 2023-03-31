957a673f49c5f9a71d618ec28b88165f767ced82
Vitor <vitorcostadev@gmail.com>
2023-03-31 16:04:45 -0300

    new implementations

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -4,50 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
+
 {
-    public function contato(Request $request) {
+    public function contato(Request $request)
+    {
 
-        /*
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        */
+        $motivo_contatos = MotivoContato::all();
 
-        /*
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
-
-        // print_r($contato->getAttributes());
-        $contato->save();
-        */
-
-        // $contato = new SiteContato();
-        // $contato->create($request->all());
-        // $contato->save();
-        // print_r($contato->getAttributes());
-
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
-    public function salvar(Request $request) {
+    public function salvar(Request $request)
+    {
+        $schema =   [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'required|email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $messages =   [
+            'nome.required' => 'O campo nome precisa ser preenchido.',
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres.',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres.',
+            'nome.unique' => 'O nome informado já está em uso.',
+
+            'telefone.required' => 'O campo telefone precisa ser preenchido.',
+
+            'motivo_contatos_id.required' => 'Selecione um motivo para o contato.',
+
+            'email' => 'O campo precisa ser preenchido com um email válido.',
+
+            'mensagem.max' => 'O campo mensagem deve ter no máximo 2000 caracteres.',
+
+            'required' => 'O campo :attribute precisa ser preenchido.'
+        ];
+
+
 
         //realizar a validação dos dados do formulário recebidos no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
-            'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
-            'mensagem' => 'required|max:2000'
-        ]);
-        // SiteContato::create($request->all());
+        $request->validate(
+            $schema,
+            $messages
+        );
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,20 +19,31 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
-Route::get('/login', function(){return 'Login';})->name('site.login');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', 'PrincipalController@principal')
+    ->name('site.index');
 
-Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')
+->name('site.contato')
+->middleware(LogAcessoMiddleware::class);
+Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
-Route::fallback(function() {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="' . route('site.index') . '">clique aqui</a> para ir para página inicial';
 });
