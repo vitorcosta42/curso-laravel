8eeb4e33d5c7a44f42f2b203ffbe6ac3cb4969e6
vitorcosta42 <vitorcostadev@gmail.com>
2023-06-20 11:58:23 -0300

    feat: envio de email de reset password  e verificação

--- app_controle_tarefas/routes/web.php
+++ app_controle_tarefas/routes/web.php
@@ -10,10 +10,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::resource('tarefa',TarefaController::class);
+Route::get('/home', [HomeController::class, 'index'])
+->name('home')
+->middleware('verified');
+Route::resource('tarefa',TarefaController::class)
+->middleware('verified');
 Route::get('/mensagem-teste',
 function(){
     return new MensagemTesteMail();
